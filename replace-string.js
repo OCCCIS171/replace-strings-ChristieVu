145e300a8164f49d7e83c61d5f589e85a2881434
Christie Vu <124643011+SomberPepperoni@users.noreply.github.com>
2024-02-10 02:24:37 -0800

    Update replace-string.js
    Signed-off-by: Christie Vu <124643011+SomberPepperoni@users.noreply.github.com>

--- replace-string.js
+++ replace-string.js
@@ -3,7 +3,7 @@ let myString = 'Hello World!!!';
 // TODO: replace the first occurence of 'o' in `myString` defined above.
 // NOTE: DO NOT HARD CODE THE SIZE OF `myString`. When you change the value
 // of `myString`, the test should still pass.
-let newString = "Hell0 World!!!";
+let newString = myString.replace("o", "0";
 
 // DO NOT CHANGE ANYTHING BELOW THIS LINE.
 
